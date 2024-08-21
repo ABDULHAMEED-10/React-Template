e00be0ec4b1f682657f180a0372e44c0cd796627
Abdul hameed <abdulhameed000650@gmail.com>
2024-08-21 20:58:24 +0500

    React with Vite and Tailwind Config

---
+++ Vite_React_Tailwind_Template/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './pages/**/*.{html,js,jsx,ts,tsx}',
+  './components/**/*.{html,js,jsx,ts,tsx}',
+  './src/**/*.{html,js,jsx,ts,tsx}',
+];
